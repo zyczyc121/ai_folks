62deeaf490fed2fea27888ae69ba9ad248e592d9
TsinghuaKEG <ebonzhao@163.com>
2017-11-29 19:48:00 +0800

    add db setting

--- ai_folks/settings.py
+++ ai_folks/settings.py
@@ -75,17 +75,28 @@ WSGI_APPLICATION = 'ai_folks.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ai_db_dev',
-        'USER': 'postgres',
-        'PASSWORD': 'qinghuacaichan',
-        'HOST': '127.0.0.1',
-        'PORT': 5432,
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'ai_db_dev',
+            'USER': 'postgres',
+            'PASSWORD': 'qinghuacaichan',
+            'HOST': '127.0.0.1',
+            'PORT': 5432,
+        }
+    }
 
 
 # Password validation
